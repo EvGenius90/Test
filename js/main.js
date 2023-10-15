8c68b4a3e5bdf5d7d1b0189056d846c0b85635d1
Evgeniy kuzmenko <kuzmenko.evgeniy2011@yandex.ru>
2023-10-15 19:20:00 +0300

    new elements

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 "use strict"
 
-console.log('good')
+// document.body.style.overflow = 'hidden'
+
+// document.addEventListener('click', function(e){
+//     if(e.target.closest('.main')){
+//         console.log('good')
+//     }
+// })
+
+let mCont = document.querySelector('.main_content')
+let block = document.querySelector('.block')
+
+document.querySelector('.addy').addEventListener('click', function(){
+    let a = document.createElement('block')
+    a.append('good')
+    mCont.append(a)
+})
